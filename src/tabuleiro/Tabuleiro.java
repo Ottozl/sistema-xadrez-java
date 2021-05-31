ee4be206bf856b67948782ee9d27c34ddbe7f4d7
Ottozl <ottoa7x@gmail.com>
2021-05-31 20:10:47 -0300

    Exceções do Tabuleiro e programação defensiva

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -5,34 +5,59 @@ public class Tabuleiro {
 	private int linhas;
 	private int colunas;
 	private Peca[][] pecas;
+
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new TabuleiroException(
+					"Erro ao criar o tabuleiro: É necessário que haja no mínimo 1 linha e 1 coluna!");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
 	}
+
 	public int getLinhas() {
 		return linhas;
 	}
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
+
 	public int getColunas() {
 		return colunas;
 	}
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-	
+
 	public Peca peca(int linha, int coluna) {
+		if (!posicaoExistente(linha, coluna)) {
+			throw new TabuleiroException("Posição não existente no tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
-	
+
 	public Peca peca(Posicao posicao) {
+		if (!posicaoExistente(posicao)) {
+			throw new TabuleiroException("Posição não existente no tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
-	
+
 	public void posicionarPeca(Peca peca, Posicao posicao) {
+		if (posicaoPreenchida(posicao)) {
+			throw new TabuleiroException("A posição " + posicao + " já está preenchida!");
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+
+	private boolean posicaoExistente(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+
+	public boolean posicaoExistente(Posicao posicao) {
+		return posicaoExistente(posicao.getLinha(), posicao.getColuna());
+	}
+
+	public boolean posicaoPreenchida(Posicao posicao) {
+		if (!posicaoExistente(posicao)) {
+			throw new TabuleiroException("Posição não existente no tabuleiro");
+		}
+		return peca(posicao) != null;
 	}
 }
